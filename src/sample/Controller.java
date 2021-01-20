0ac75bf129aa8218d642e4b5e12b810d0cafa603
Nguyen Ngoc Khanh <nguyenngockhanhbl@gmail.com>
2021-01-20 22:05:32 +0700

    ADD validator form

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,40 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public Button addButton;
+
+    public void addButtonClick(ActionEvent e) throws IOException {
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("new_timetable_form.fxml"));
+        Parent newTimetableFormParent = fxmlLoader.load();
+        Scene scene = new Scene(newTimetableFormParent);
+
+        Stage dialog = new Stage();
+        dialog.setScene(scene);
+        dialog.setTitle("New Timetable Form");
+        dialog.initOwner(stage);
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.showAndWait();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
